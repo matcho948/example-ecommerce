610758b1c8be6c2b25e0b21492855aa3746695b3
matcho948 <83008970+matcho948@users.noreply.github.com>
2023-06-01 12:56:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,21 @@ terraform {
     access_key = "Cvo5/+XUlhisJMfCby1o5SqWdinai0sGps9vqQdGghq5iFzoILEhNrkpj6yyg2JvmWKFF82DF0vO+AStXCMf7g=="
   }
 }
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "myAppServicePlan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "myWebApp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
+
