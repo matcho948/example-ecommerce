9a634b62bbc6e80dd8a65da80810f6f8c280ef00
matcho948 <83008970+matcho948@users.noreply.github.com>
2023-06-01 12:37:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "testGroupT"
   location = "Poland Central"
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "MyResourceGroup"
+    storage_account_name = "myresource17124"
+    container_name       = "test"
+    key                  = "prod.terraform.tfstate"
+  }
+}
