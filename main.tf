876b8e51a0139b7677959a13f270495934cee528
matcho948 <83008970+matcho948@users.noreply.github.com>
2023-06-02 10:10:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,3 +68,18 @@ resource "azurerm_sql_database" "example" {
   edition             = "Basic"
 }
 
+resource "azurerm_storage_account" "example" {
+  name                     = "webappstorageaccount1464481"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+}
+
+resource "azurerm_storage_container" "example" {
+  name = "webappcontainer"
+  storage_account_name = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
