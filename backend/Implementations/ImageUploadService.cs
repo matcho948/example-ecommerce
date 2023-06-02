12b6db08970b23655add3e04b009e2c332ee40db
Mateusz Chojnacki <mateusz.chojnacki@q-perior.com>
2023-06-02 11:17:01 +0200

    Implement jpg

--- backend/Implementations/ImageUploadService.cs
+++ backend/Implementations/ImageUploadService.cs
@@ -12,9 +12,9 @@ public class ImageUploadService : IImageStorageService
     public async Task<string> UploadImageAsync(Guid imageId, Stream imageStream)
     {
         BlobContainerClient blobContainerClient = new BlobContainerClient("DefaultEndpointsProtocol=https;AccountName=appstorageaccount144481;AccountKey=Lpl4xjwM240PWZFpGaLWXh7CzTHbYoGLi0Z4JoVyo5Mg97C1oinpMNUaWj8tX5C8/Acsxv7eGrkN+AStji6HRw==;EndpointSuffix=core.windows.net", "webappcontainer");
-        BlobClient blob = blobContainerClient.GetBlobClient(imageId.ToString());
+        BlobClient blob = blobContainerClient.GetBlobClient(imageId.ToString() + ".jpg");
         await blob.UploadAsync(imageStream, true);
         imageStream.Dispose();
-        return blob.Uri.ToString();
+        return new Uri(blob.Uri.ToString());
     }
 }
